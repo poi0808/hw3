01e04b6ab236cbd11b680d23d5b4edd39e2ce607
Alex Sung <gihyeok.sung@kellogg.northwestern.edu>
2024-02-18 16:40:39 +0000

    2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,22 +4,20 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @places = Place.find by(params => "id")
+    @place = Place.find(params[:id])
   end
 
   def new
-    @places = Place.new
+    @place = Place.new
   end
 
   def create
     @place = Place.new
-    @place["title"] = params ["title"]
-    @place["date"] = params ["date"]
-    @place["description"] = params ["description"]
+    @place.title = params[:title]
+    @place.date = params[:date]
+    @place.description = params[:description]
 
     @place.save
-    redirect_to "/places"
-    
-    end
+    redirect_to places_path
   end
 end

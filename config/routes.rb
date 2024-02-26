8aa9172ad1b139c17ec60e33bfcc135bd53f2a64
Alex Sung <gihyeok.sung@kellogg.northwestern.edu>
2024-02-26 04:44:46 +0000

    12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :places
-    resources :entries
-    root 'places#index'
+  resources "places"
+  resources "entries"
+  root 'places#index'
 end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })

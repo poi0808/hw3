36f9da1ff054945f3bdc87fd5a69781b7de9571f
Alex Sung <gihyeok.sung@kellogg.northwestern.edu>
2024-02-26 04:13:14 +0000

    11

---
+++ app/controllers/entries_contoller.rb
@@ -0,0 +1,24 @@
+class EntriesController < ApplicationController
+
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => @entry["place_id"]})
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new
+
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+end
